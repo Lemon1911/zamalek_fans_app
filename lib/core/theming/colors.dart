264ee1eef588f83a3e381fed6af15397449c8220
Lemon1911 <m7moud.laymouna.ml@gmail.com>
2024-06-25 20:14:04 +0300

    -Added validation to login & register screens(Email, password, Name).  -Added animation to (onboarding, login and register screens).  --Updating Text form field custom widget and App directional button widget.  -Added package for animation which is animate_do.  -Added some colors for scaffold background.

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -4,8 +4,9 @@ class ColorsManager {
   static const Color lightRed1 = Color(0xffffcbd1);
   static const Color lightRed2 = Color(0xfff69697);
   static const Color lightRed3 = Color(0xffee6b6e);
-  static const Color lightRed4 = Color(0xffff2c2c);
+  static const Color lightRed4 = Color(0xfff5f5f6);
   static const Color lightRed5 = Color(0xffde0a26);
   static const Color lightRed6 = Color(0xffc30010);
   static const Color mainWhite = Color(0xF5F5F6FF);
+  static const Color backGroundMain = Color(0xFff7f7f7);
 }
